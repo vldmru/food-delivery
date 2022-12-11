abcc8b23d4e23dcba1f450fe45063883aa3b0326
vldmru <103037144+vldmru@users.noreply.github.com>
2022-12-11 23:23:30 +0000

    Deploying to gh-pages from @ vldmru/food-delivery@b49b46b0538c9263c69eb584b509e3a0f55f4eb2 🚀

---
+++ index.d0960b6c.js
@@ -0,0 +1,2 @@
+!function(){var e={openModalBtn:document.querySelector("[data-modal-open2]"),closeModalBtn:document.querySelector("[data-modal-close2]"),modal:document.querySelector("[data-modal2]")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)}();
+//# sourceMappingURL=index.d0960b6c.js.map
